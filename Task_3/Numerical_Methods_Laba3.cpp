bc18f6c2969d07a780ff34e047d2ce1d094a5751
twrkkk <zhenya.shein.03@gmail.com>
2023-12-20 20:11:30 +0300

    update

--- Task_3/Numerical_Methods_Laba3.cpp
+++ Task_3/Numerical_Methods_Laba3.cpp
@@ -133,53 +133,6 @@ std::vector<std::vector<double>> multiply(std::vector<std::vector<double>>& matr
 	return matrix;
 }
 
-std::vector<double> multiplyMatrixVector(const std::vector<std::vector<double>>& matrix, const std::vector<double>& vector) {
-	if (matrix[0].size() != vector.size()) {
-		std::cerr << "Некорректные размеры матрицы и вектора для умножения." << std::endl;
-		exit(EXIT_FAILURE);
-	}
-
-	size_t rows = matrix.size();
-	size_t cols = matrix[0].size();
-
-	std::vector<double> result(rows, 0.0);
-
-	for (size_t i = 0; i < rows; ++i) {
-		for (size_t j = 0; j < cols; ++j) {
-			result[i] += matrix[i][j] * vector[j];
-		}
-	}
-
-	return result;
-}
-
-double calculateVectorNorma(const std::vector<double>& vec) {
-	double result = 0.0;
-
-	for (const auto& element : vec) {
-		result = max(result, abs(element));
-	}
-
-	return result;
-}
-
-std::vector<double> subtractVectors(std::vector<double> vector1, std::vector<double> vector2) {
-	if (vector1.size() != vector2.size()) {
-		std::cerr << "Некорректные размеры векторов для вычитания." << std::endl;
-		exit(EXIT_FAILURE);
-	}
-
-	size_t size = vector1.size();
-
-	std::vector<double> result(size);
-
-	for (size_t i = 0; i < size; ++i) {
-		result[i] = vector1[i] - vector2[i];
-	}
-
-	return result;
-}
-
 void printMatrix(const std::vector<std::vector<double>>& matrix) {
 	for (const auto& row : matrix) {
 		for (double element : row) {
@@ -189,7 +142,7 @@ void printMatrix(const std::vector<std::vector<double>>& matrix) {
 	}
 }
 
-std::vector<std::vector<double>> generateMatrixWithLambda(vector<double> l, double& norma)
+std::vector<std::vector<double>> generateMatrixWithLambda(vector<double> l)
 {
 	int N = l.size();
 	auto w = generateRandomVector(N);
@@ -199,18 +152,12 @@ std::vector<std::vector<double>> generateMatrixWithLambda(vector<double> l, doub
 	auto right = multiplyMatrices(multiply(matrix_w, 2), w_trasposed);
 	auto hausholder = subtractMatrices(e, right);
 
-	cout << '\n' << " Матрица Хаусхолдера " << '\n';
+	/*cout << '\n' << " Матрица Хаусхолдера " << '\n';
 	printMatrix(hausholder);
-	cout << '\n';
+	cout << '\n';*/
 
 	auto lambda = generateMatrixWithDiagonal(l, N);
 	auto symmetricMatrix = multiplyMatrices(multiplyMatrices(hausholder, lambda), transposeMatrix(hausholder));
-
-	auto left = multiplyMatrixVector(symmetricMatrix, hausholder[0]);
-	auto r = multiplyMatrixVector(lambda, hausholder[0]);
-
-	auto vector = subtractVectors(left, r);
-	norma = calculateVectorNorma(vector);
 
 	return symmetricMatrix;
 }
@@ -269,32 +216,30 @@ vector<double> jacobi_rotation(std::vector<std::vector<double>>& A, int n, doubl
 	}
 }
 
-vector<tuple<double, double>> test(int n, int l_min, int l_max, double eps, int& max_iteration, double& max_aij, double& norma_sum)
+vector<tuple<double, double>> test(int n, int l_min, int l_max, double eps, int& max_iteration, double& max_aij)
 {
 	vector<double> expected_lambda(n);
 	for (int i = 0; i < n; ++i)
 		expected_lambda[i] = l_min + static_cast<double>(rand()) / RAND_MAX * (l_max - l_min);
 
-	double norma = 0;
-	auto symmetricMatrix = generateMatrixWithLambda(expected_lambda, norma);
-	norma_sum += norma;
-	cout << " Сгенерированная матрица " << '\n';
+	auto symmetricMatrix = generateMatrixWithLambda(expected_lambda);
+	/*cout << " Сгенерированная матрица " << '\n';
 	printMatrix(symmetricMatrix);
-	cout << '\n';
+	cout << '\n';*/
 
 	auto real_lambda = jacobi_rotation(symmetricMatrix, n, eps, max_iteration, max_aij);
 
 	sort(expected_lambda.begin(), expected_lambda.end());
 	sort(real_lambda.begin(), real_lambda.end());
 
-	cout << " Ожидаемые собственные значения " << '\n';
+	/*cout << " Ожидаемые собственные значения " << '\n';
 	for (int i = 0; i < n; ++i)
 		cout << expected_lambda[i] << ' ';
 	cout << '\n';
 	cout << " Полученные собственные значения " << '\n';
 	for (int i = 0; i < n; ++i)
 		cout << real_lambda[i] << ' ';
-	cout << '\n';
+	cout << '\n';*/
 
 	vector<tuple<double, double>> result;
 	for (int i = 0; i < n; ++i)
@@ -306,12 +251,12 @@ vector<tuple<double, double>> test(int n, int l_min, int l_max, double eps, int&
 int main() {
 	srand(static_cast<unsigned int>(time(nullptr)));
 
-	int iteration = 1;
+	int iteration = 10;
 
 	vector<tuple<int, int, int, double, int>> test_cases
 	{
-		make_tuple(3, -2, 2, 1e-5, 2000),
-		/*make_tuple(10, -2, 2, 1e-7, 2000),
+		make_tuple(10, -2, 2, 1e-5, 2000),
+		make_tuple(10, -2, 2, 1e-7, 2000),
 		make_tuple(10, -2, 2, 1e-9, 2000),
 		make_tuple(10, -50, 50, 1e-5, 2000),
 		make_tuple(10, -50, 50, 1e-7, 2000),
@@ -322,7 +267,7 @@ int main() {
 		make_tuple(30, -2, 2, 1e-9, 2000),
 		make_tuple(30, -50, 50, 1e-5, 2000),
 		make_tuple(30, -50, 50, 1e-7, 2000),
-		make_tuple(30, -50, 50, 1e-9, 2000),*/
+		make_tuple(30, -50, 50, 1e-9, 2000),
 	};
 
 	SetConsoleCP(1251);
@@ -335,7 +280,6 @@ int main() {
 		<< std::setw(25) << "Максимальное значение A_ij"
 		<< std::setw(25) << "Среднее число итераций"
 		<< std::setw(25) << "Средняя оценка точности /\\"
-		<< std::setw(25) << "Средняя мера точности"
 		<< '\n';
 
 	for (int c = 0; c < test_count; ++c)
@@ -356,7 +300,7 @@ int main() {
 			double max_delta = -1e20;
 			max_aij = -1e20;
 			max_iteration = get<4>(test_cases[c]);
-			auto result = test(n, l_min, l_max, eps, max_iteration, max_aij, norma_sum);
+			auto result = test(n, l_min, l_max, eps, max_iteration, max_aij);
 
 			sum_of_iteration += max_iteration;
 
@@ -376,7 +320,6 @@ int main() {
 			<< std::setw(25) << max_aij
 			<< std::setw(25) << sum_of_iteration / iteration
 			<< std::setw(25) << sum_of_delta / iteration
-			<< std::setw(25) << norma_sum / iteration
 			<< "\n\n";
 	}
 
